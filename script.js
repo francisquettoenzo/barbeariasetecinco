940b4d4973d148dacaec2291592f8aa176fb6f1e
Enzo Francisquetto Ribeiro <124091470+francisquettoenzo@users.noreply.github.com>
2026-01-13 00:41:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,35 @@ const prevBtn = document.querySelector('.carousel-btn.prev');
 const nextBtn = document.querySelector('.carousel-btn.next');
 
 let index = 0;
-const imageWidth = 407; // largura + gap
+let imageWidth = getImageWidth();
+
+function getImageWidth() {
+    const width = window.innerWidth;
+
+    if (width <= 600) {
+        return width * 0.91; // mobile
+    } else if (width <= 1024) {
+        return 390; // tablet
+    } else {
+        return 210; // desktop
+    }
+}
+
+function updateCarousel() {
+    imageWidth = getImageWidth();
+    track.style.transform = `translateX(-${index * imageWidth}px)`;
+}
 
 nextBtn.addEventListener('click', () => {
     index++;
     if (index > track.children.length - 1) index = 0;
-    track.style.transform = `translateX(-${index * imageWidth}px)`;
+    updateCarousel();
 });
 
 prevBtn.addEventListener('click', () => {
     index--;
     if (index < 0) index = track.children.length - 1;
-    track.style.transform = `translateX(-${index * imageWidth}px)`;
+    updateCarousel();
 });
 
+window.addEventListener('resize', updateCarousel);
