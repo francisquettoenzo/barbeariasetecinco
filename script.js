b64eec826948fc640313b28520a35bf354b7c7c2
Enzo Francisquetto Ribeiro <124091470+francisquettoenzo@users.noreply.github.com>
2026-01-13 00:13:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const track = document.querySelector('.carousel-track');
+const prevBtn = document.querySelector('.carousel-btn.prev');
+const nextBtn = document.querySelector('.carousel-btn.next');
+
+let index = 0;
+const imageWidth = 407; // largura + gap
+
+nextBtn.addEventListener('click', () => {
+    index++;
+    if (index > track.children.length - 1) index = 0;
+    track.style.transform = `translateX(-${index * imageWidth}px)`;
+});
+
+prevBtn.addEventListener('click', () => {
+    index--;
+    if (index < 0) index = track.children.length - 1;
+    track.style.transform = `translateX(-${index * imageWidth}px)`;
+});
+
